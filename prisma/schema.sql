babca43524004f130a216349a899c5e64f3bab06
alexnakagawa <alexnakagawa3@gmail.com>
2020-12-03 21:25:43 -0800

    change schema to account for olympedia

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "public"."User" (
+CREATE TABLE IF NOT EXISTS "public"."User" (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE "public"."Post" (
+CREATE TABLE IF NOT EXISTS "public"."Post" (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
@@ -14,14 +14,14 @@ CREATE TABLE "public"."Post" (
   FOREIGN KEY ("authorId") REFERENCES "public"."User"(id)
 );
 
-CREATE TABLE "public"."Profile" (
+CREATE TABLE IF NOT EXISTS "public"."Profile" (
   id SERIAL PRIMARY KEY NOT NULL,
   bio TEXT,
   "userId" INTEGER UNIQUE NOT NULL,
   FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
 );
 
-CREATE TABLE "public"."Athlete" (
+CREATE TABLE IF NOT EXISTS "public"."Athlete" (
   id SERIAL PRIMARY KEY NOT NULL,
   "firstName" VARCHAR(255),
   "lastName" VARCHAR(255),
@@ -31,22 +31,28 @@ CREATE TABLE "public"."Athlete" (
   status VARCHAR(255)
 );
 
-CREATE TABLE "public"."Sport" (
-  id SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS "public"."AthleteMap" (
+  athleteId INTEGER REFERENCES "public"."Athlete" (id),
+  espnId INTEGER,
+  olympediaId INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS "public"."Sport" (
+  id VARCHAR(3) PRIMARY KEY NOT NULL,
   sport VARCHAR(255) NOT NULL,
   gender VARCHAR(255),
   olympic BOOLEAN
 );
 
 -- 3NF
-CREATE TABLE "public"."AthleteHeritage" (
+CREATE TABLE IF NOT EXISTS "public"."AthleteHeritage" (
   "athleteId" INTEGER NOT NULL,
   "countryId" VARCHAR(2) NOT NULL,
   FOREIGN KEY ("athleteId") REFERENCES "public"."Athlete"(id),
   PRIMARY KEY ("athleteId", "countryId")
 );
 
-CREATE TABLE "public"."FavoriteMoments" (
+CREATE TABLE IF NOT EXISTS "public"."FavoriteMoments" (
   id SERIAL PRIMARY KEY NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
   "userId" INTEGER REFERENCES "public"."User" (id),
