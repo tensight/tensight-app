dd00831a182c2a9c56e0df76a5dd0976b7f58531
alexnakagawa <alexnakagawa3@gmail.com>
2020-12-03 21:25:24 -0800

    prisma calls now async

--- lib/athletes.ts
+++ lib/athletes.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export function getSortedAthletesData() {
-  const athletes = prisma.athlete.findMany();
-  return athletes
+export async function getSortedAthletesData() {
+  const athletes = await prisma.athlete.findMany();
+  console.log(athletes);
+  return athletes;
 }
