f749f266691ee13bad277eb94aaf94f16173238a
alexnakagawa <alexnakagawa3@gmail.com>
2021-01-11 00:02:03 -0800

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-center border-t border-grey p-4 pin-b">
+      <p className="font-serif">Made with ☕️ &nbsp;by <a href="http://twitter.com/nakaflocka3">Alex</a></p>
+      <p className="font-serif">© 2021 Tensight</p>
+    </footer>
+  );
+}
