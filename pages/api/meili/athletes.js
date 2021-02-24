f8222b310d275ad28a21fefff58a561ae49da12e
alexnakagawa <alexnakagawa3@gmail.com>
2021-02-23 18:37:25 -0800

    POST and GET methods with try/catch wraps

--- pages/api/meili/athletes.js
+++ pages/api/meili/athletes.js
@@ -2,30 +2,41 @@ const MeiliSearch = require('meilisearch');
 import { PrismaClient } from '@prisma/client';
 
 export default async (req, res) => {
-    try {
-        const prisma = new PrismaClient();
-        const athletes = await prisma.athlete.findMany();
+    const {
+        query: { q }
+    } = req;
 
-        const client = new MeiliSearch({
-            host: process.env.MEILISEARCH_HOST,
-            apiKey: process.env.MEILISEARCH_API_KEY,
+    const client = new MeiliSearch({
+        host: process.env.MEILISEARCH_HOST,
+        apiKey: process.env.MEILISEARCH_API_KEY,
+    });
+
+    if (!client)
+        return res.status(500).json({
+            error: new Error('Missing MeiliSearch environment variables'),
         });
 
-        if (!client)
-            return res.status(500).json({
-                error: new Error('Missing MeiliSearch environment variables'),
-            });
+    if (req.method === 'POST') {
+        try {
+            const prisma = new PrismaClient();
+            const athletes = await prisma.athlete.findMany();
+            const index = await client.getOrCreateIndex('athletes');
+            index.addDocuments(athletes).then((res) => console.log(res));
+            res.status(201).json({'index':'athletes'});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error });
+        }
+    } else {
+        try {
+            const index = await client.getOrCreateIndex('athletes');
+            const search = await index.search(q);
+
+            res.json(search);
             
-        const index = await client.getOrCreateIndex('athletes');
-        index.addDocuments(athletes).then((res) => console.log(res));
-
-        const query = 'jp'; // search query, in this case we want all athletes with the letter n
-        const search = await index.search(query);
-
-        res.json(search);
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error });
+        }
     }
 };
