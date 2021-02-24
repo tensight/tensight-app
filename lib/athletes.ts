0b435efb6a11f31dcfcd0692991ec8a5928063e8
alexnakagawa <alexnakagawa3@gmail.com>
2021-02-23 18:32:56 -0800

    add golf as possible option

--- lib/athletes.ts
+++ lib/athletes.ts
@@ -40,13 +40,15 @@ export function getEspnLeague(sportId: string) {
       return 'mlb';
     case 'HOK':
       return 'nhl';
+    case 'GOL':
+      return 'golf'
     default:
       return '';
   }
 };
 
-const availableLeagues = ['BAS', 'TEN', 'AFB', 'BSB', 'HOK']
-const workaroundLeagues = ['FTB']
+const availableLeagues = ['BAS', 'TEN', 'AFB', 'BSB', 'HOK', 'GOL'];
+const workaroundLeagues = ['FTB'];
 export function getAthleteHeadshot(espnId: number, sportId: string) {
   if (espnId !== null && availableLeagues.some(l => l === sportId)) {
     return `https://a.espncdn.com/combiner/i?img=/i/headshots/${getEspnLeague(sportId)}/players/full/${espnId}.png`
