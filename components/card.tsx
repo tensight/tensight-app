b33483c1df4583bb6f3aafcc3050c1722b6342cc
alexnakagawa <alexnakagawa3@gmail.com>
2021-03-05 11:57:04 -0800

    remove id number for debugging

--- components/card.tsx
+++ components/card.tsx
@@ -12,7 +12,6 @@ const Card: React.FC<{athlete: Athlete}> = ({ athlete }) => {
       <a>
         <article id="card" className="bg-white flex relative space-x-4 flex-col w-64 p-5 min-w-64 rounded-l transition duration-500 ease-in-out transform hover:-translate-y-2 hover:-translate-x-1 hover:shadow-xl hover:rotate-1 motion-reduce:transform-none" >
         <header id="card-header">
-          <h2 className="text-lg">{athlete.id}</h2>
           <div className="mx-auto">
             <img className="w-full h-48 object-contain"
               src={getAthleteHeadshot(athlete.espnId, athlete.sport)}
