2f756ba7b76dcc6c6afad2786e5c6a617fba961a
alexnakagawa <alexnakagawa3@gmail.com>
2021-03-06 23:06:19 -0800

    nav bar contains signin option

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useSession } from 'next-auth/client'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://github.com/tensight', label: 'GitHub' },
-  // { href: 'https://nextjs.org/docs', label: 'Login' },
-]
-
 const Nav: React.FC = () => {
+  const [session, loading] = useSession()
+
+  const links = [
+    { href: 'https://github.com/tensight', label: 'GitHub' },
+  ]
+
+  const getLinksJSX = (links) => {
+    session
+      ? links.push({ href: '/profile', label: 'Profile' })
+      : links.push({ href: '/api/auth/signin', label: 'Sign in' })
+    return links.map(({ href, label }) => (
+      <li key={`${href}${label}`}>
+        <Link href={href}>
+          <a className="btn-purple no-underline">
+            {label}
+          </a>
+        </Link>
+      </li>
+    ))
+  }
+
   return (
     <nav>
       <ul className="flex justify-between items-center p-8">
@@ -16,14 +33,7 @@ const Nav: React.FC = () => {
           </Link>
         </li>
         <ul className="flex justify-between items-center space-x-4">
-          {links.map(({ href, label }) => (
-            
-            <li key={`${href}${label}`}>
-              <a href={href} className="btn-purple no-underline">
-                {label}
-              </a>
-            </li>
-          ))}
+          {getLinksJSX(links)}
         </ul>
       </ul>
     </nav>
