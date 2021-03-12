61aa854635ce4a42db751635b86c1db424bcc7cf
alexnakagawa <alexnakagawa3@gmail.com>
2021-03-11 23:20:57 -0800

    add temporary profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,17 @@
+
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <style jsx global>{`
+          body {
+            background-color: #e5e5e3;
+          }
+        `}
+      </style>
+      <p>Coming soon...</p>
+    </div>
+  )
+}
+
+export default ProfilePage
