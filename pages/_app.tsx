61c4b52e6600affd01d77af37c0a0e4c08091a0f
alexnakagawa <alexnakagawa3@gmail.com>
2021-03-14 22:45:24 -0700

    add SWRCofig wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/index.css'
 import { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
+import { SWRConfig } from 'swr'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          refreshInterval: 10000,
+          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
     </Provider>
   )
 }
