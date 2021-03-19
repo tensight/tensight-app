613922665fed7b5d7f7d3c291b89ee425d8ce8b7
alexnakagawa <alexnakagawa3@gmail.com>
2021-03-18 22:42:38 -0700

    add twitter profile pics to image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   images: {
-    domains: ['a.espncdn.com', 'cdn.discordapp.com', 'futwiz.com', 'i.imgur.com', 'img.buymeacoffee.com', 'media.giphy.com'],
+    domains: [
+      'a.espncdn.com', 
+      'cdn.discordapp.com', 
+      'futwiz.com', 
+      'i.imgur.com', 
+      'img.buymeacoffee.com', 
+      'media.giphy.com',
+      'pbs.twimg.com'],
   },
 }
