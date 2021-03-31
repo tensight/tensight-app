8db6a386d0b29f46111344ff8371ee99e93ef137
alexnakagawa <alexnakagawa3@gmail.com>
2021-03-30 23:56:22 -0700

    fix logo issue #1

--- components/nav.tsx
+++ components/nav.tsx
@@ -59,7 +59,7 @@ const Nav = () => {
         <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
           <div className="px-5 pt-4 flex items-center justify-between">
             <div>
-              <img className="h-8 w-auto" src="./tensight.svg" alt="Tensight Logo" />
+              <img className="h-8 w-auto" src="/tensight.svg" alt="Tensight Logo" />
             </div>
               <div className="-mr-2">
                 <button 
