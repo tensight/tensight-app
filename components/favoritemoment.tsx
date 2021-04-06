5c3d069caa1aeb42762e9c741cc0227c6a2906c0
alexnakagawa <alexnakagawa3@gmail.com>
2021-04-05 23:50:10 -0700

    :lipstick:  fix spacing issues and ui shadows

--- components/favoritemoment.tsx
+++ components/favoritemoment.tsx
@@ -8,7 +8,12 @@ type Props = {
 
 const FavoriteMoment = ({ favMoment }: Props) => {
   return (
-    <div className="flex">
+    <div className="flex mx-2 p-2 bg-white bg-opacity-10 rounded-xl shadow-lg md:mx-3"
+      style={{
+        backdropFilter: 'blur(10px)',
+      }}
+    >
+
       <div className="p-2">
         <div className="">
           <Image
@@ -19,7 +24,7 @@ const FavoriteMoment = ({ favMoment }: Props) => {
             className="rounded"
           />
         </div>
-        <div className="">
+        <div className="px-1">
           <span>{favMoment.description}</span>
         </div>
         <div className="flex items-center">
@@ -28,7 +33,7 @@ const FavoriteMoment = ({ favMoment }: Props) => {
               <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
             </svg>
             <div className="ml-1">
-              <span className="text-gray-700">(x) favorited this.</span>
+              <span className="text-gray-700 truncate">(x) favorited this.</span>
             </div>
           </div>
           <div className="text-base font-normal mx-2 my-5 sm:mt-2 sm:mb-4">·</div>
