61b4a9a2e79a4e32156e83f9880134dd448f7c46
alexnakagawa <alexnakagawa3@gmail.com>
2021-05-01 23:51:45 -0700

    :art:  flex-col on smaller screens, more awesome structural changes

--- components/athleteheader.tsx
+++ components/athleteheader.tsx
@@ -1,6 +1,7 @@
 import { getAthleteHeadshot } from '@/lib/athletes'
 import { getFlag, getCountryName } from '@/lib/countries'
 import { Athlete, Sport } from '@prisma/client'
+import Image from 'next/image'
 
 type Props = {
   athleteData: Athlete,
@@ -9,37 +10,34 @@ type Props = {
 
 const AthleteHeader = ({athleteData, sportOfAthlete}: Props) => {
   return (
-    <div className="w-auto lg:w-3/5">
-      <div className="text-xl m-8">
-        <div className="flex flex-row items-center w-full p-2 border-solid border-4 border-gray-700 border-opacity-50">
-          <div className="m-1 min-w-min">
-            <img className="h-48 object-contain"
-                 src={getAthleteHeadshot(athleteData.headshotUrl, athleteData.espnId, athleteData.sport)}
-                 onError={(e) => {
-                  (e.target as HTMLImageElement).onerror = null;
-                  (e.target as HTMLImageElement).src = "./athlete.png";
-                 }}
-            ></img>
+    <div className=" text-xl m-8 flex flex-col md:flex-row items-center w-auto p-2 border-solid border-4 border-gray-700 border-opacity-50 rounded-md lg:w-3/5">
+      <div id="athleteHeadshot" className="m-1 min-w-[180px]">
+        <Image
+          src={getAthleteHeadshot(athleteData.headshotUrl, athleteData.espnId, athleteData.sport)}
+          width={240}
+          height={180}
+          layout="intrinsic"
+          objectFit="contain"
+        >
+        </Image>
+      </div>
+      <div id="athleteVitals" className="flex flex-col justify-between h-48 m-1 py-2">
+          <div className="p-0 m-0">
+            <h1 className="font-serif font-semibold text-4xl">{`${athleteData.firstName} ${athleteData.lastName}`}</h1>
           </div>
-          <div id="athleteVitals" className="flex flex-col justify-between h-48 m-1 py-2">
-            <div className="p-0 m-0">
-              <h1 className="font-serif font-semibold text-4xl">{`${athleteData.firstName} ${athleteData.lastName}`}</h1>
-            </div>
-            <div>
-              <h3 className="font-serif text-gray-600">{athleteData.nativeName == null ? '' : `(${athleteData.nativeName})`}</h3>
-            </div>
-            <div>
-              <h3 className="font-serif text-gray-600">{athleteData.description == null ? '' : athleteData.description}</h3>
-            </div>
-            <div className="font-serif flex">
-              Heritage: {convertFlagsToJSX(athleteData.heritage)}
-            </div>
-            <div>
-              <h3 className="font-serif">Sport: {sportOfAthlete.sport}</h3>
-            </div>
+          <div className={athleteData.nativeName == null ? "hidden" : "font-serif text-gray-600"}>
+            <h3 className="font-serif text-gray-600">{athleteData.nativeName == null ? '' : `(${athleteData.nativeName})`}</h3>
+          </div>
+          <div>
+            <h3 className="font-serif text-gray-600">{athleteData.description == null ? '' : athleteData.description}</h3>
+          </div>
+          <div className="font-serif flex">
+            Heritage: {convertFlagsToJSX(athleteData.heritage)}
+          </div>
+          <div>
+            <h3 className="font-serif">Sport: {sportOfAthlete.sport}</h3>
           </div>
         </div>
-      </div>
     </div>
   )
 }
