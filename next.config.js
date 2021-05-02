7cb9eb44cc88c7745f1d82f08a395522312318cf
alexnakagawa <alexnakagawa3@gmail.com>
2021-05-01 18:02:37 -0700

    :sparkles: add more valid image domains

--- next.config.js
+++ next.config.js
@@ -7,11 +7,16 @@ module.exports = {
     domains: [
       'ak-static.cms.nba.com',
       'a.espncdn.com', 
-      'cdn.discordapp.com', 
+      'cdn.discordapp.com',
+      'external-content.duckduckgo.com',
       'futwiz.com', 
       'i.imgur.com', 
-      'img.buymeacoffee.com', 
+      'img.buymeacoffee.com',
+      'media.gettyimages.com',
       'media.giphy.com',
-      'pbs.twimg.com'],
-  },
+      'media.vanityfair.com',
+      'pbs.twimg.com',
+      'upload.wikimedia.org'
+    ],
+  },   
 }
